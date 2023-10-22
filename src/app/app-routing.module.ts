530eb0a30625f76f43e349af397b038ef1543e1c
Jefte Franco <jeftefranco@outlook.com>
2023-10-22 00:46:05 -0600

    Ruta 'login' y 'principal' agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CuadriculaPrincipalComponent } from './cuadricula-principal/cuadricula-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'principal', component: CuadriculaPrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/menu-superior/menu-superior.component.ts
+++ src/app/menu-superior/menu-superior.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-superior',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-superior.component.css']
 })
 export class MenuSuperiorComponent {
-
+  @Input() showButtons: boolean = true;
 }
